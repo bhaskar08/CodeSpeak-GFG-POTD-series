b2a3678123bbd2111a93599e387c6e37ca97c19c
Bhaskar <114566320+bhaskar08@users.noreply.github.com>
2023-01-21 13:25:52 +0530

    Update code.cpp

--- 21th Jan Minimum X (xor) A/code.cpp
+++ 21th Jan Minimum X (xor) A/code.cpp
@@ -2,16 +2,23 @@
 
 class Solution {
   public:
+  int countSetBit(int num)
+  {
+      int setBitsb=0;
+      while(num)
+        {
+            setBitsb+=num%2;
+            num/=2;
+        }
+        
+        return setBitsb;
+  }
     int minVal(int a, int b) {
         // code here
-        int temp=b;
-        int setBitsb=0;
+
+        int setBitsb=countSetBit(b);
         
-        while(temp)
-        {
-            setBitsb+=temp%2;
-            temp/=2;
-        }
+        
         
         int ans=0;
         int setBitAns=setBitsb;
